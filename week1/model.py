86001faf253830a8a7e9fe89f8113a8de7248d62
Jeroen van der Laan <jeroen.jeroenvdlaan@gmail.com>
2019-09-17 08:58:42 +0200

    tussencommit

--- week1/model.py
+++ week1/model.py
@@ -25,7 +25,7 @@ class PriorityQueue:
     # pop returns the smallest item from the heap
     # i.e. the root element = element (priority, item) with highest priority
     def get(self):
-        return heapq.heappop(self.elements)[1]
+        return heapq.heappop(self.elements)
 
 def bernoulli_trial(app):
     return 1 if random.random() < int(app.prob.get())/10 else 0
@@ -35,31 +35,49 @@ def get_grid_value(node):
     return grid[node[0]][node[1]]
 
 # Return alle aanliggende nodes die binnen het bord vallen.
-def get_adjacent_nodes(node):
+def get_adjacent_nodes(node, visited):
     x, y = node
-    adjacents = []
-    if x+1 < cf.SIZE:
-        adjacents.append((x+1, y))
-    if y+1 < cf.SIZE:
-        adjacents.append((x, y+1))
-    if x-1 >= 0:
-        adjacents.append((x-1, y))
-    if y-1 >= 0:
-        adjacents.append((x, y-1))
-    return adjacents
+    candidates = []
+    candidates.append((x+1, y))
+    candidates.append((x, y+1))
+    candidates.append((x-1, y))
+    candidates.append((x, y-1))
+
+    neighbors = []  # alle valid neighbors
+    for x, y in candidates:
+        if x < 0 or x >= cf.SIZE:
+            continue
+        if y < 0 or y >= cf.SIZE:
+            continue
+        if grid[x][y] == 'b':
+            continue
+        if (x,y) in visited:
+            continue
+        neighbors.append((x,y))
+    return neighbors
 
 def UCS(app, start, goal):
     # plot a sample path for demonstration
     queue = PriorityQueue()
-    for neighbor in get_adjacent_nodes(start):
-        queue.put(neighbor, get_grid_value(neighbor))
+    visited = []
+    queue.put(start, 1)     # zet de start node in de queue
+    weight = 0
+    path = [start,]
+    while(len(queue.elements) > 0):     # zolang er nog items in de queue zitten:
+        current_node = queue.get()      # get the top node from the queue
+        visited.append(current_node[1])
+        # finish requirement
+        if current_node[1] == cf.GOAL:
+            print("path found!")
+            return
 
-        app.plot_line_segment(i, i, i, i+1, color=cf.FINAL_C)
+        print("current node:", current_node)
+        for neighbor in get_adjacent_nodes(current_node[1], visited):       # vindt de aanliggende nodes
+            print("neighbours:", neighbor)
+            queue.put(neighbor, current_node[0] - 1)                # Zet de neighbor in de queue met een priority van -1 (?)
 
-        print("i:", i)
-        if grid[i][i+1] == 'b':
-            app.plot_node((i, i), 'blue')
-        app.plot_line_segment(i, i+1, i+1, i+1, color=cf.FINAL_C)
+        # app.plot_line_segment(i, i, i, i+1, color=cf.FINAL_C)
+        #app.plot_node((i, i), 'blue')
         app.pause()
 
 def A_star(app, start, goal):
