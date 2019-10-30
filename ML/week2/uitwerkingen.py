47eb84ffea28faff68dfdb48950167a86696c27a
Jeroen van der Laan <jeroen.jeroenvdlaan@gmail.com>
2019-10-30 10:37:52 +0100

    ?

--- ML/week2/uitwerkingen.py
+++ ML/week2/uitwerkingen.py
@@ -2,16 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.sparse import csr_matrix
 
-
 # ==== OPGAVE 1 ====
 def plotNumber(nrVector):
     # Let op: de manier waarop de data is opgesteld vereist dat je gebruik maakt
-    # van de Fortran index-volgorde – de eerste index verandert het snelst, de 
-    # laatste index het langzaamst; als je dat niet doet, wordt het plaatje 
-    # gespiegeld en geroteerd. Zie de documentatie op 
+    # van de Fortran index-volgorde – de eerste index verandert het snelst, de
+    # laatste index het langzaamst; als je dat niet doet, wordt het plaatje
+    # gespiegeld en geroteerd. Zie de documentatie op
     # https://docs.scipy.org/doc/numpy/reference/generated/numpy.reshape.html
-
-    nrVector = np.reshape(nrVector, (20, 20), 'F')  # Mogelijk 3e param 'F' megeven voor Fortran index volgorde
+    nrVector = np.reshape(nrVector, (20,20), 'F')
     plt.matshow(nrVector)
     plt.show()
 
@@ -22,8 +20,7 @@ def sigmoid(z):
     # voor dat de code zowel werkt wanneer z een getal is als wanneer z een
     # vector is.
     # Maak gebruik van de methode exp() in NumPy.
-
-    return 1 / (1 + np.exp(-z))
+    return 1/(1+np.exp(-z))
 
 
 # ==== OPGAVE 2b ====
@@ -33,32 +30,31 @@ def get_y_matrix(y, m):
     # Let op: de gegeven vector y is 1-based en de gevraagde matrix is 0-based,
     # dus als y_i=1, dan moet regel i in de matrix [1,0,0, ... 0] zijn, als
     # y_i=10, dan is regel i in de matrix [0,0,...1] (in dit geval is de breedte
-    # van de matrix 10 (0-9), maar de methode moet werken voor elke waarde van 
+    # van de matrix 10 (0-9), maar de methode moet werken voor elke waarde van
     # y en m
-
-    # elke waarde in y-1 is de index voor 1 en de rest is 0
-    A = [1 for i in range(len(y))]
-    groepering = [i for i in
-                  range(len(y) + 1)]  # Row in de CSR (voor elke waarde in y moet er een row zijn, dus len(y))
-    indices = np.reshape(y - 1, m)  # zet 1-based y om in 0-based JA -> dit is column in de CSR
-    return csr_matrix((A, indices, groepering)).todense()
+    rows = [i for i in range(len(y) + 1)]
+    columns = np.reshape(y - 1, m)
+    data = [1 for i in y]
+    y_vec = csr_matrix((data, columns, rows)).todense()
+    return y_vec
 
 
-# ==== OPGAVE 2c ==== 
+# ==== OPGAVE 2c ====
 # ===== deel 1: =====
 def predictNumber(Theta1, Theta2, X):
     # Deze methode moet een matrix teruggeven met de output van het netwerk
-    # gegeven de waarden van Theta1 en Theta2. Elke regel in deze matrix 
+    # gegeven de waarden van Theta1 en Theta2. Elke regel in deze matrix
     # is de waarschijnlijkheid dat het sample op die positie (i) het getal
     # is dat met de kolom correspondeert.
 
     # De matrices Theta1 en Theta2 corresponderen met het gewicht tussen de
     # input-laag en de verborgen laag, en tussen de verborgen laag en de
-    # output-laag, respectievelijk. 
+    # output-laag, respectievelijk.
 
     # Een mogelijk stappenplan kan zijn:
 
     #    1. voeg enen toe aan de gegeven matrix X; dit is de input-matrix a1
+    a1 = np.c_(len(X), X)
     #    2. roep de sigmoid-functie van hierboven aan met a1 als actuele
     #       parameter: dit is de variabele a2
     #    3. voeg enen toe aan de matrix a2, dit is de input voor de laatste
@@ -68,30 +64,20 @@ def predictNumber(Theta1, Theta2, X):
 
     # Voeg enen toe aan het begin van elke stap en reshape de uiteindelijke
     # vector zodat deze dezelfde dimensionaliteit heeft als y in de exercise.
-
-    a1 = np.c_[np.ones(len(X)), X]
-    a2 = np.transpose(sigmoid(np.dot(Theta1, np.transpose(a1))))
-    a3 = np.c_[np.ones(len(a2)), a2]
-    a4 = sigmoid(np.dot(Theta2, np.transpose(a3)))
-
-    return np.transpose(a4)
+    pass
 
 
 # ===== deel 2: =====
 def computeCost(Theta1, Theta2, X, y):
     # Deze methode maakt gebruik van de methode predictNumber() die je hierboven hebt
-    # geïmplementeerd. Hier wordt het voorspelde getal vergeleken met de werkelijk 
+    # geïmplementeerd. Hier wordt het voorspelde getal vergeleken met de werkelijk
     # waarde (die in de parameter y is meegegeven) en wordt de totale kost van deze
     # voorspelling (dus met de huidige waarden van Theta1 en Theta2) berekend en
     # geretourneerd.
-    # Let op: de y die hier binnenkomt is de m×1-vector met waarden van 1...10. 
+    # Let op: de y die hier binnenkomt is de m×1-vector met waarden van 1...10.
     # Maak gebruik van de methode get_y_matrix() die je in opgave 2a hebt gemaakt
-    # om deze om te zetten naar een matrix. 
-
-    y_vec = get_y_matrix(y, len(y))
-    predictions = predictNumber(Theta1, Theta2, X)
-    return (1/len(y))*sum( sum((-y_vec*np.transpose(np.log(predictions)) - ((1-y_vec)*np.transpose(np.log(1-predictions))))))
-
+    # om deze om te zetten naar een matrix.
+    pass
 
 # ==== OPGAVE 3a ====
 def sigmoidGradient(z):
@@ -100,6 +86,19 @@ def sigmoidGradient(z):
     # scalaire waarden en met vectoren.
     return sigmoid(z)*(1-sigmoid(z))
 
+
+# ==== OPGAVE 3b ====
+def nnCheckGradients(Theta1, Theta2, X, y):
+    pass
+
+
+
+# ==== OPGAVE 3a ====
+def sigmoidGradient(z):
+    # Retourneer hier de waarde van de afgeleide van de sigmoïdefunctie.
+    # Zie de opgave voor de exacte formule. Zorg ervoor dat deze werkt met
+    # scalaire waarden en met vectoren.
+    pass
 
 # ==== OPGAVE 3b ====
 def nnCheckGradients(Theta1, Theta2, X, y):
@@ -132,5 +131,4 @@ def nnCheckGradients(Theta1, Theta2, X, y):
 
     Delta2_grad = Delta2 / m
     Delta3_grad = Delta3 / m
-
     return Delta2_grad, Delta3_grad
